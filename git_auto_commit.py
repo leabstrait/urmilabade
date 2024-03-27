495f83ed29ea7c0f780e280ff8a3943f5440ac8c
Labin Ojha <leabstrait@gmail.com>
2024-03-27 20:11:45 +0000

    autocommit - update repo

--- git_auto_commit.py
+++ git_auto_commit.py
@@ -1,6 +1,9 @@
 import subprocess
 
-def git_commit_push(commit_message="autocommit - update repo"):
+
+def git_commit_push(commit_message):
+    commit_message = commit_message or "autocommit - update repo"
+
     # Commit changes
     subprocess.run(["git", "add", "."])
     subprocess.run(["git", "commit", "-m", commit_message])
@@ -10,6 +13,7 @@ def git_commit_push(commit_message="autocommit - update repo"):
 
     # Push changes
     subprocess.run(["git", "push"])
+
 
 if __name__ == "__main__":
     commit_message = input("Enter the commit message: ")
